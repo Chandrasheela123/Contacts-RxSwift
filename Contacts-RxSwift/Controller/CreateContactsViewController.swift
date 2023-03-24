501d72a0e71150d86f08d501464801e58966cf27
Chandrasheela123 <chandrasheela1927@gmail.com>
2023-03-24 18:05:23 +0530

    Added create contacts screen

---
+++ Contacts-RxSwift/Controller/CreateContactsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateContactsViewController.swift
+//  Contacts-RxSwift
+//
+//  Created by Chandrasheela Hotkar on 24/03/23.
+//
+
+import UIKit
+
+class CreateContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
