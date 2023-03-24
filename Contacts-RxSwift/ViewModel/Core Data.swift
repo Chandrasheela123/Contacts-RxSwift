b8cccdf401d786cbff2508059fb5aba55325cc9d
prama0301 <niyogiprama@gmail.com>
2023-03-24 14:24:26 +0530

    Folders Added

---
+++ Contacts-RxSwift/ViewModel/Core Data.swift
@@ -0,0 +1,8 @@
+//
+//  Core Data.swift
+//  Contacts-RxSwift
+//
+//  Created by prama niyogi on 24/03/23.
+//
+
+import Foundation
---
+++ Contacts-RxSwift/ViewModel/DetailModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailModel.swift
+//  Contacts-RxSwift
+//
+//  Created by prama niyogi on 24/03/23.
+//
+
+import Foundation
