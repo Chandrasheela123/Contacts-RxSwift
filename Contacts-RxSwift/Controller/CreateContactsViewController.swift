174198767df50a03313bf8eccf78772fca3d2c01
Chandrasheela123 <chandrasheela1927@gmail.com>
2023-03-27 14:44:45 +0530

    Added core data

--- Contacts-RxSwift/Controller/CreateContactsViewController.swift
+++ Contacts-RxSwift/Controller/CreateContactsViewController.swift
@@ -6,16 +6,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class CreateContactsViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
 
+    @IBOutlet weak var emailIDTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var contactNumberTextField: UITextField!
+    @IBOutlet weak var contactNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Home Dir: \(NSHomeDirectory())")
+        bindDataToTextFields()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func bindDataToTextFields() {
+        
+        contactNameTextField.rx.text.bind(to: ContactDetailsViewModel.contactDetailsInstance.nameSubject).disposed(by: disposeBag)
+        
+        contactNumberTextField.rx.text.bind(to: ContactDetailsViewModel.contactDetailsInstance.phoneNumberSubject).disposed(by: disposeBag)
+        emailIDTextField.rx.text.bind(to: ContactDetailsViewModel.contactDetailsInstance.emailSubject).disposed(by: disposeBag)
+        
+        
+    }
+    
+    @IBAction func createButton(_ sender: Any) {
+        
+        let name = contactNameTextField.text ?? ""
+        let email = emailIDTextField.text ?? ""
+        let phoneNumber = contactNumberTextField.text ?? ""
+        
+        let result = AddContactDetails().saveDetails(name: name, email: email, phoneNum: phoneNumber)
+        print(result)
+        
+        
+    }
+    @IBAction func cancleButton(_ sender: Any) {
+    }
+    @IBAction func switchButton(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Contacts-RxSwift/Utilities.swift
@@ -0,0 +1,10 @@
+//
+//  Utilities.swift
+//  Contacts-RxSwift
+//
+//  Created by Chandrasheela Hotkar on 27/03/23.
+//
+
+import Foundation
+import UIKit
+
